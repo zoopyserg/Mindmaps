ae18338bf74a9dffaa3dcb230e8083cc80c96e04
Serge Vinogradoff <1587149+zoopyserg@users.noreply.github.com>
2022-12-24 00:06:37 +0200

    added a file

---
+++ Old Mindmaps/Vinogradoff Group/Дневник повторений/Simpler Newer Journal/assets/js/Ludens---WYSIWYG-Summernote-Editor.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#summernote').summernote({
+		placeholder: 'Descripción de las certificaciones y logros del ponente.',
+		height: 300,                 // set editor height
+		minHeight: null,             // set minimum height of editor
+		maxHeight: null,             // set maximum height of editor
+		focus: false                 // set focus to editable area after initializing summernote
+	});
+});
